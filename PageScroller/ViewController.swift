5c765d053f22428545e635e3bd2b23cadd76a0cd
zmilles <zmilles@gmail.com>
2017-06-10 19:50:49 -0400

    Uploading contents

--- PageScroller/ViewController.swift
+++ PageScroller/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = Array<UIImageView>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0
+        view.addGestureRecognizer(scrollView.panGestureRecognizer)
+        
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = (scrollView.frame.size.width/2 +
+                scrollView.frame.size.width * CGFloat(x))
+            
+            contentWidth += scrollView.frame.size.width
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2)
+                - 75, width: 150, height: 150)
+        }
+        
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
 
 }
 
